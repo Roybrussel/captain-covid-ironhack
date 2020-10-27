45c0804824982710d55da86a2b7f8c08403bf875
teof95 <teof95@gmail.com>
2020-10-27 15:33:49 +0100

    added player and virus images

--- src/enemycreate.js
+++ src/enemycreate.js
@@ -7,31 +7,39 @@ let numEnemies = 75;
 let enemiesForce = 0.001;
 // HIGHER DIFFICULTIES
 if (difficulty === 2) {
-  interval = 1000;
-  numEnemies = 125;
-  enemiesForce = 0.0015;
+    interval = 1000;
+    numEnemies = 125;
+    enemiesForce = 0.0015;
 } else if (difficulty === 3) {
-  interval = 750;
-  numEnemies = 200;
-  enemiesForce = 0.002;
+    interval = 750;
+    numEnemies = 200;
+    enemiesForce = 0.002;
 }
 
 // CREATE ENEMIES
 const enemies = [];
 for (let i = 0; i < numEnemies; i++) {
-  oddEvenCounter++;
-  enemies.push(Bodies.circle(playArea(), 0, sizeW / 30));
+    oddEvenCounter++;
+    enemies.push(
+        Bodies.circle(playArea(), 0, sizeW / 30, {
+            render: {
+                sprite: {
+                    texture: "/images/covid.png",
+                    xScale: 0.5,
+                    yScale: 0.5
+                },
+            },
+        })
+    );
 }
 
 // ADD ENEMIES TO WORLD AND LAUNCH ATTACK
 enemies.forEach((enemy, index) => {
-  enemy.frictionAir = 0;
-  setTimeout(() => {
-    World.add(world, enemy);
-    Body.applyForce(
-      enemy,
-      { x: enemy.position.x, y: enemy.position.y },
-      { x: 0, y: (sizeW / 100) * enemiesForce }
-    );
-  }, index * interval);
+    enemy.frictionAir = 0;
+    setTimeout(() => {
+        World.add(world, enemy);
+        Body.applyForce(
+            enemy, { x: enemy.position.x, y: enemy.position.y }, { x: 0, y: (sizeW / 100) * enemiesForce }
+        );
+    }, index * interval);
 });
