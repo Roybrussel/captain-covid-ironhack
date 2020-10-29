0e560f6097105b5b10fcbdbe7021db6907684c0f
Rich Alossi <alossix@gmail.com>
2020-10-29 13:43:13 +0100

    Score button flashing effect

--- src/collisions.js
+++ src/collisions.js
@@ -1,28 +1,35 @@
 // OFF-SCREEN DETECTION & DELETION
-const isOffScreen = function() {
-    let offScreenCheck = setInterval(() => {
-        for (i = 3; i < world.bodies.length; i++) {
-            // REMOVE VIRUS FROM WORLD AFTER REACHING BOTTOM OF SCREEN
-            if (world.bodies[i].position.y > sizeH) {
-                World.remove(world, world.bodies[i]);
-                enemiesRemaining--;
-                if (scoreCount > 0) {
-                    scoreCount -= 5;
-                    showHighscore[0].innerHTML = scoreCount;
-                    // INSERT SCORECOUNT BUTTON FLASH HERE FOR -5 POINTS
-                }
-                if (enemiesRemaining <= 0) {
-                    setTimeout(() => {
-                        stopWorld();
-                    }, 3000);
-                }
-            }
-            // REMOVE AMMO FROM WORLD AFTER REACHING TOP OF SCREEN
-            if (world.bodies[i].position.y < -200) {
-                World.remove(world, world.bodies[i]);
-            }
+let highscoreBtn = document.querySelector(".highscore");
+const isOffScreen = function () {
+  let offScreenCheck = setInterval(() => {
+    for (i = 3; i < world.bodies.length; i++) {
+      // REMOVE VIRUS FROM WORLD AFTER REACHING BOTTOM OF SCREEN
+      if (world.bodies[i].position.y > sizeH) {
+        World.remove(world, world.bodies[i]);
+        enemiesRemaining--;
+        if (scoreCount > 0) {
+          scoreCount -= 5;
+          showHighscore[0].innerHTML = scoreCount;
+          // INSERT SCORECOUNT BUTTON FLASH HERE FOR -5 POINTS
+          highscoreBtn.style.backgroundColor = "white";
+          highscoreBtn.style.color = "black";
+          setTimeout(() => {
+            highscoreBtn.style.backgroundColor = "#dc3545";
+            highscoreBtn.style.color = "white";
+          }, 300);
         }
-    }, 1000);
+        if (enemiesRemaining <= 0) {
+          setTimeout(() => {
+            stopWorld();
+          }, 3000);
+        }
+      }
+      // REMOVE AMMO FROM WORLD AFTER REACHING TOP OF SCREEN
+      if (world.bodies[i].position.y < -200) {
+        World.remove(world, world.bodies[i]);
+      }
+    }
+  }, 1000);
 };
 isOffScreen();
 
@@ -34,7 +41,6 @@ let laserShot = new Audio("sound/laser.mp3");
 const djTrump = new Audio("sound/djtrump.mp3");
 const shipExplosion = new Audio("sound/shipexplosion.mp3");
 const enemyExplosion = new Audio("sound/enemyexplosion.mp3");
-
 
 // SCORE COUNT
 let scoreCount = 0;
@@ -78,26 +84,26 @@ Events.on(engine, "collisionStart", ({ pairs }) => {
 // END OF COLLISION
 // COLLISION OF SHIP (ID 3) WITH VIRUS (ID > 3)
 Events.on(engine, "collisionEnd", ({ pairs }) => {
-    pairs.forEach(({ bodyA, bodyB }) => {
-        if (bodyA.id === 3 && bodyB.id > 3) {
-            Body.setPosition(playerBody, { x: sizeW / 2, y: sizeH - 80 });
-            Body.setVelocity(playerBody, { x: 0, y: 0 });
-            playerBody.isStatic = false;
-            numLives--;
-            if (numLives === 2) {
-                life3[0].classList = "far fa-star life3";
-            }
-            if (numLives === 1) {
-                life2[0].classList = "far fa-star life2";
-            }
-            if (numLives === 0) {
-                mainTheme.pause();
-                gameOverSound.play();
-                life1[0].classList = "far fa-star life1";
-                launchBtnIcon[0].classList = "fas fa-skull rocket-icon";
-                launchBtnText.innerText = "RETRY";
-                stopWorld();
-            }
-        }
-    });
+  pairs.forEach(({ bodyA, bodyB }) => {
+    if (bodyA.id === 3 && bodyB.id > 3) {
+      Body.setPosition(playerBody, { x: sizeW / 2, y: sizeH - 80 });
+      Body.setVelocity(playerBody, { x: 0, y: 0 });
+      playerBody.isStatic = false;
+      numLives--;
+      if (numLives === 2) {
+        life3[0].classList = "far fa-star life3";
+      }
+      if (numLives === 1) {
+        life2[0].classList = "far fa-star life2";
+      }
+      if (numLives === 0) {
+        mainTheme.pause();
+        gameOverSound.play();
+        life1[0].classList = "far fa-star life1";
+        launchBtnIcon[0].classList = "fas fa-skull rocket-icon";
+        launchBtnText.innerText = "RETRY";
+        stopWorld();
+      }
+    }
+  });
 });
